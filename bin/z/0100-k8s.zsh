ebefb90e0e56ba2e3cbeb6f473bb2a8059633229
berquerant <sin@sin-pro.local>
2023-09-28 21:10:13 +0900

    Apply diff

--- bin/z/0100-k8s.zsh
+++ bin/z/0100-k8s.zsh
@@ -19,8 +19,8 @@ kustomize_diff() {
     right_kustomized="$(mktemp)"
     kustomize_sorted "$left" | yq "$query_left" > "$left_kustomized"
     kustomize_sorted "$right" | yq "$query_right" > "$right_kustomized"
-    left_name="${left} ${left_query}"
-    right_name="${right} ${right_query}"
+    left_name="${left} ${query_left}"
+    right_name="${right} ${query_right}"
     diff -u "$left_kustomized" "$right_kustomized" | sed -e "s|${left_kustomized}|${left_name}|" -e "s|${right_kustomized}|${right_name}|"
 }
 
@@ -46,8 +46,8 @@ kustomize_diff_between_branch() {
     right_sha="$(git rev-parse --short HEAD)"
     kustomize_sorted "$target" | yq "$query_right" > "$right_kustomized"
 
-    left_name="[${left}] ${target} ${left_query}"
-    right_name="[${right}] ${target} ${right_query}"
+    left_name="[${left}] ${target} ${query_left}"
+    right_name="[${right}] ${target} ${query_right}"
     diff -u "$left_kustomized" "$right_kustomized" | sed -e "s|${left_kustomized}|${left_name}|" -e "s|${right_kustomized}|${right_name}|"
 
     git switch "$original_branch"
