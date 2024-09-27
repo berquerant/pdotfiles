1554ca29b330ebeda4afe94a511cb57b81ab0cf0
berquerant <sin@sin-pro.local>
2024-09-27 14:51:09 +0900

    Apply diff

--- bin/z/0100-k8s.zsh
+++ bin/z/0100-k8s.zsh
@@ -7,6 +7,13 @@ kgetall() {
     kubectl get "$(kubectl api-resources --namespaced=true --verbs=list --output=name | xargs | tr ' ' ',')" $@
 }
 
-kgetev() {
-    kubectl get event --sort-by='{.lastTimestamp}' $@
+kgetevent() {
+    # kgetevent [MINUTE] [KUBECTL_OPT...]
+    local minute="${1:-10}"
+    local second="$(($minute*60))"
+    shift
+    kubectl get event --sort-by='.lastTimestamp' $@  -o json |\
+        jq --arg now $(date +%s) \
+           --arg second $second \
+           '.items[] | select((.lastTimestamp | fromdate) > (($now | tonumber) - ($second | tonumber)))' -c
 }
