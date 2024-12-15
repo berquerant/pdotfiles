31ff719c4ce81ad03e5e55d791066054c20ac999
berquerant <sin@sin-pro.local>
2024-12-15 15:15:10 +0900

    Apply diff

--- bin/z/0200-etc.zsh
+++ bin/z/0200-etc.zsh
@@ -1,17 +1,24 @@
 #!/bin/zsh
 
-MPV_SETTINGS_ZSH="${DOTFILES_ROOT}/ivg/repos/mpv-settings/mpv.zsh"
+MPV_SETTINGS_SH="${DOTFILES_ROOT}/ivg/repos/mpv-settings/mpv.sh"
 
 __mpv_installed() {
-    type mpv >/dev/null 2>&1 && [ -f "$MPV_SETTINGS_ZSH" ]
+    type mpv >/dev/null 2>&1 && [ -f "$MPV_SETTINGS_SH" ]
 }
 
 load_mpv() {
-    source "$MPV_SETTINGS_ZSH"
+    . "$MPV_SETTINGS_SH"
 }
 
 if __mpv_installed ; then
     load_mpv
+fi
+
+if which fflist > /dev/null 2>&1 && [ -n "$MUSIC_ROOT" ]; then
+    ffquery() {
+        # ffquery QUERY... | mpv-play
+        fflist query -r "$MUSIC_ROOT" "$@"
+    }
 fi
 
 histfile_stat() {
