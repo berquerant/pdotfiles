7a766d381abea6651dec9f73a4846cf8d6e9e80d
berquerant <sin@sin-pro.local>
2025-08-09 17:47:53 +0900

    Apply diff

--- bin/install-via-git-go.sh
+++ bin/install-via-git-go.sh
@@ -2,24 +2,15 @@
 
 set -xe
 
-docker version
-
 readonly goos="darwin"
 readonly goarch="amd64"
-readonly docker_go_image="golang:${GO_VERSION}"
-readonly repo="https://github.com/berquerant/install-via-git-go.git"
-readonly reponame="install-via-git-go"
-readonly repod="${PJTMP}/${reponame}"
 readonly location="/usr/local/bin/install-via-git"
-readonly ref="v0.17.2"
+readonly ref="0.18.0"
+readonly url="https://github.com/berquerant/install-via-git-go/releases/download/v${ref}/install-via-git_${ref}_${goos}_${goarch}"
 
-if [ ! -d "$repod" ] ; then
-    git clone "$repo" "$repod"
+if [[ -x "$location" && "$("$location" version | grep Version)" == "Version: v${ref}" ]] ; then
+    exit
 fi
-cd "$repod"
-git checkout main || true
-git fetch
-git pull --prune --force origin main
-git checkout "$ref"
-docker run --rm -v "$repod":/usr/src/app -w /usr/src/app -e "GOOS=${goos}" -e "GOARCH=${goarch}" "$docker_go_image" go build -v -o dist/install-via-git
-ln -snvf "${repod}/dist/install-via-git" "$location"
+
+curl -L -o "$location" "$url"
+chmod +x "$location"
