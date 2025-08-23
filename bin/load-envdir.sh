7e71c0999e3ccd455e37ed4d603af4e84792ca5b
berquerant <sin@sin-pro.local>
2025-08-23 18:18:57 +0900

    Apply diff

--- bin/load-envdir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-while read -r line ; do
-    export "$line"
-done < "${DOTFILES_ROOT}/.envdir"
--- bin/zload.zsh
+++ bin/zload.zsh
@@ -2,6 +2,15 @@
 
 zload() {
     source "${DOTFILES_ROOT}/.env"
-    "${DOTFILES_ROOT}/bin/load-envdir.sh"
+    export EMACSD="${HOME}/.emacs.d"
+    export CMACSD="${HOME}/.cmacs.d"
+    export TMPD="${HOME}/tmp"
+    export GOENV_ROOT="${HOME}/.goenv"
+    export GOPATH="${HOME}/go"
+    export PYENV_ROOT="${HOME}/.pyenv"
+    export RBENV_ROOT="${HOME}/.rbenv"
+    export NVM_DIR="${HOME}/.nvm"
+    export NPM_ROOT="${HOME}/.npm"
+    export CARGO_HOME="${HOME}/.cargo"
     find "${DOTFILES_ROOT}/bin/z" -type f | sort | while read line ; do source "$line" ; done
 }
